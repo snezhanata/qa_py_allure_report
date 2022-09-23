b73ce3d068ac84f89961cccc49063ea13a66b862
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-09-24 00:17:38 +0400

    Initial commit

---
+++ tests/test_selene.py
@@ -0,0 +1,13 @@
+from selene import by, be
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s, ss
+
+
+def test_dynamic_steps():
+    browser.open('https://github.com')
+    s('.header-search-input').click()
+    s('.header-search-input').send_keys('snezhanata/qa_python_day_8_allure_report')
+    s('.header-search-input').submit()
+    s(by.link_text('snezhanata/qa_python_day_8_allure_report')).click()
+    s('#issues-tab').click()
+    s(by.partial_text('don\'t_fade')).should(be.visible)
