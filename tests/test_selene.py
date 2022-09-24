1f4e200fc5a706d933c23d695a062995d8abf38c
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-09-25 00:49:03 +0400

    text changes

--- tests/test_selene.py
+++ tests/test_selene.py
@@ -1,3 +1,5 @@
+# Task 1: Чистый Selene (без шагов)
+
 from selene import by, be
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s, ss
--- tests/test_steps.py
+++ tests/test_steps.py
@@ -1,3 +1,6 @@
+# Task 2: Лямбда шаги через with allure.step
+# Task 3: Шаги с декоратором @allure.step
+
 import allure
 from selene import by, be
 from selene.support.shared import browser
@@ -19,21 +22,30 @@ def test_dynamic_steps():
 
 
 def test_decorator_steps():
-    @allure.step('Launch browser and open a page')
-    def open_page():
-        browser.open('https://github.com')
+    open_page()
+    search_repository()
+    navigation_to_tab()
+    find_issue_by_name()
 
-    @allure.step('Search needful repository')
-    def search_repository():
-        s('.header-search-input').click()
-        s('.header-search-input').send_keys('snezhanata/qa_python_day_8_allure_report')
-        s('.header-search-input').submit()
 
-    @allure.step('Navigation through repository to find "Issues" tab')
-    def navigation_to_tab():
-        s(by.link_text('snezhanata/qa_python_day_8_allure_report')).click()
-        s('#issues-tab').click()
+@allure.step('Launch browser and open a page')
+def open_page():
+    browser.open('https://github.com')
 
-    @allure.step('Find an appropriate issue by name')
-    def find_issue_by_name():
-        s(by.partial_text('tab doesn\'t open')).should(be.visible)
+
+@allure.step('Search needful repository')
+def search_repository():
+    s('.header-search-input').click()
+    s('.header-search-input').send_keys('snezhanata/qa_python_day_8_allure_report')
+    s('.header-search-input').submit()
+
+
+@allure.step('Navigation through repository to find "Issues" tab')
+def navigation_to_tab():
+    s(by.link_text('snezhanata/qa_python_day_8_allure_report')).click()
+    s('#issues-tab').click()
+
+
+@allure.step('Find an appropriate issue by name')
+def find_issue_by_name():
+    s(by.partial_text('tab doesn\'t open')).should(be.visible)
