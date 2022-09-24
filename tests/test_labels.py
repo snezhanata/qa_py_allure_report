8c5115487ccff74559b152674818a98c48b5de0e
snezhanata <111530556+snezhanata@users.noreply.github.com>
2022-09-25 01:23:40 +0400

    text changes

--- tests/test_labels.py
+++ tests/test_labels.py
@@ -5,11 +5,11 @@ from allure_commons.types import Severity
 
 
 def test_dynamic_labels():
-    allure.dynamic.tag
+    allure.dynamic.tag('normal')
     allure.dynamic.severity(Severity.NORMAL)
     allure.dynamic.feature('List of tasks in the repository')
     allure.dynamic.story('Unauthorized user shouldn\'t create a task')
-    allure.dynamic.link('https://github.com', name='Testing')
+    allure.dynamic.link('https://github.com/snezhanata/qa_python_day_8_allure_report', name='Repository')
     pass
 
 
@@ -18,6 +18,6 @@ def test_dynamic_labels():
 @allure.label('owner', 'Snezhana')
 @allure.feature('List of tasks in the repository')
 @allure.story('User creates a task')
-@allure.link('https://github.com', name='Testing')
+@allure.link('https://github.com', name='Repository')
 def test_decorator_labels():
     pass
--- tests/test_steps_and_labels.py
+++ tests/test_steps_and_labels.py
@@ -1,12 +1,18 @@
 # Task 2: Лямбда шаги через with allure.step
 # Task 3: Шаги с декоратором @allure.step
+# Task 4: Разметка тестов всеми аннотациями
 
 import allure
 from selene import by, be
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s, ss
 
-
+def test_dynamic_labels():
+    allure.dynamic.tag('normal')
+    allure.dynamic.severity(Severity.NORMAL)
+    allure.dynamic.feature('List of tasks in the repository')
+    allure.dynamic.story('Unauthorized user shouldn\'t create a task')
+    allure.dynamic.link('https://github.com/snezhanata/qa_python_day_8_allure_report', name='Repository')
 def test_dynamic_steps():
     with allure.step('Launch browser and open a page'):
         browser.open('https://github.com')
@@ -21,6 +27,12 @@ def test_dynamic_steps():
         s(by.partial_text('tab doesn\'t open')).should(be.visible)
 
 
+@allure.tag('trivial')
+@allure.severity(Severity.TRIVIAL)
+@allure.label('owner', 'Snezhana')
+@allure.feature('List of tasks in the repository')
+@allure.story('User creates a task')
+@allure.link('https://github.com', name='Repository')
 def test_decorator_steps():
     open_page()
     search_repository()
